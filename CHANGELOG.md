6552f884b4be53c738968eac0a8abea0c081712d
Rahul Bhargava <rahulbot@gmail.com>
2026-01-13 12:26:30 -0500

    docs(release): bump for release and notes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ Change Log
 
 Here is a history of what was changed in each version. 
 
+### v4.9.0
+
+* support longer newsdata.io queries (up to 512) now that we paid them more
+
 ### v4.8.8
 
 * tweak Newscatcher fetch rate to stay under api quota
--- processor/__init__.py
+++ processor/__init__.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 from sentry_sdk import init
 from sentry_sdk.integrations.logging import ignore_logger
 
-VERSION = "4.8.8"
+VERSION = "4.9.0"
 SOURCE_GOOGLE_ALERTS = "google-alerts"
 SOURCE_MEDIA_CLOUD = "media-cloud"
 SOURCE_NEWSCATCHER = "newscatcher"
